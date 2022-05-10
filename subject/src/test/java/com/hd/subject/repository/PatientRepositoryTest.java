a415656ce75cdc444255b3992c0793de291b8059
정주헌 <cherish8513@naver.com>
2022-05-10 13:57:43 +0900

    feat: Patient Repository, Visit Repository 테스트 코드 추가

--- subject/src/test/java/com/hd/subject/repository/PatientRepositoryTest.java
+++ subject/src/test/java/com/hd/subject/repository/PatientRepositoryTest.java
@@ -3,6 +3,8 @@ package com.hd.subject.repository;
 import static org.junit.jupiter.api.Assertions.*;
 
 import com.hd.subject.domain.Patient;
+import com.hd.subject.domain.status.GenderCode;
+import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
@@ -15,8 +17,23 @@ class PatientRepositoryTest {
 
   @Test
   public void savePatient() throws Exception{
-      //given
-      //when
-      //then
+    //given
+    Patient patient = Patient.builder()
+        .birthday("2000.01.01")
+        .genderCode(GenderCode.CODE_M)
+        .name("name")
+        .phoneNumber("010-1234-5678")
+        .registrationNumber("01-00002-0003")
+        .build();
+    //when
+    Patient savedPatient = patientRepository.save(patient);
+
+    //then
+    Assertions.assertThat(savedPatient.getId()).isNotNull();
+    Assertions.assertThat(savedPatient.getHospital()).isNull();
+    Assertions.assertThat(savedPatient.getBirthday()).isEqualTo("2000.01.01");
+    Assertions.assertThat(savedPatient.getName()).isEqualTo("name");
+    Assertions.assertThat(savedPatient.getPhoneNumber()).isEqualTo("010-1234-5678");
+    Assertions.assertThat(savedPatient.getRegistrationNumber()).isEqualTo("01-00002-0003");
   }
 }
